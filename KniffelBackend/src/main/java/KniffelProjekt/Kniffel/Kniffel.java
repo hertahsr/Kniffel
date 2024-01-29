e4c5e76d3eb6d05eca48f62f853d860ccbf1934e
njuergensen <n.juergensen@hotmail.de>
2024-01-29 14:52:11 +0100

    Getter und Setter mit Lombok implementiert, Initialisierung angepasst, Attribut namen überarbeitet

--- KniffelBackend/src/main/java/KniffelProjekt/Kniffel/Kniffel.java
+++ KniffelBackend/src/main/java/KniffelProjekt/Kniffel/Kniffel.java
@@ -1,87 +1,42 @@
 package KniffelProjekt.Kniffel;
 
 import KniffelProjekt.Spieler.Spieler;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Kniffel {
-
+    private Long id;
     private List<Spieler> teilnehmer;
-
-    private Spieler aktiverSpieler;
-
+    private int aktiverSpielerIndex = 0;
     private int runde = 0;
-
     private int uebrigeWuerfe = 3;
+    private List<Integer> wuerfel = new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1));
 
-    private List<Integer> aktiveWuerfel= new ArrayList<Integer>(5);
-
-    private List<Integer> fixeWuerfel = new ArrayList<Integer>();
 
     // Konstruktor
-    public Kniffel(List<Spieler> teilnehmer) {
-        this.teilnehmer = teilnehmer;
-        this.aktiverSpieler = teilnehmer.get(0);
-    }
+//    public Kniffel(List<Spieler> teilnehmer) {
+//        this.teilnehmer = teilnehmer;
+//        this.aktiverSpieler = teilnehmer.get(0);
+//    }
 
-    public List<Integer> wuerfeln() {
-        List<Integer> retval = new ArrayList<Integer>();
-        for (int i = 0; i < aktiveWuerfel.size(); i++) {
-            int zahl = ThreadLocalRandom.current().nextInt(1, 7);
-            retval.set(i, zahl);
-        }
-        return retval;
-    }
-
-
-    // Getter & Setter
-    public List<Spieler> getTeilnehmer() {
-        return teilnehmer;
-    }
-
-    public void setTeilnehmer(List<Spieler> teilnehmer) {
-        this.teilnehmer = teilnehmer;
-    }
-
-    public Spieler getAktiverSpieler() {
-        return aktiverSpieler;
-    }
-
-    public void setAktiverSpieler(Spieler aktiverSpieler) {
-        this.aktiverSpieler = aktiverSpieler;
-    }
-
-    public int getRunde() {
-        return runde;
-    }
-
-    public void setRunde(int runde) {
-        this.runde = runde;
-    }
-
-    public int getUebrigeWuerfe() {
-        return uebrigeWuerfe;
-    }
-
-    public void setUebrigeWuerfe(int uebrigeWuerfe) {
-        this.uebrigeWuerfe = uebrigeWuerfe;
-    }
-
-    public List<Integer> getAktiveWuerfel() {
-        return aktiveWuerfel;
-    }
-
-    public void setAktiveWuerfel(List<Integer> aktiveWuerfel) {
-        this.aktiveWuerfel = aktiveWuerfel;
-    }
-
-    public List<Integer> getFixeWuerfel() {
-        return fixeWuerfel;
-    }
-
-    public void setFixeWuerfel(List<Integer> fixeWuerfel) {
-        this.fixeWuerfel = fixeWuerfel;
-    }
+//    public List<Integer> wuerfeln() {
+//        List<Integer> retval = new ArrayList<Integer>();
+//        for (int i = 0; i < wuerfel.size(); i++) {
+//            int zahl = ThreadLocalRandom.current().nextInt(1, 7);
+//            retval.set(i, zahl);
+//        }
+//
+//        return retval;
+//    }
 }
