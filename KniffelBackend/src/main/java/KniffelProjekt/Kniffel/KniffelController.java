0c76f174197715c1a70003a84a9c2d0b31732552
njuergensen <n.juergensen@hotmail.de>
2024-01-29 14:53:11 +0100

    neuesSpielStarten Endpunkt in Controller und Service implementiert

---
+++ KniffelBackend/src/main/java/KniffelProjekt/Kniffel/KniffelController.java
@@ -0,0 +1,20 @@
+package KniffelProjekt.Kniffel;
+
+import KniffelProjekt.Spieler.Spieler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/kniffel")
+public class KniffelController {
+
+    @Autowired
+    KniffelService kniffelService;
+
+    @PostMapping("/neuesSpielStarten")
+    public Kniffel getNeuesSpiel(@RequestBody ArrayList<Spieler> teilnehmer) {
+        return kniffelService.spielStarten(teilnehmer);
+    }
+}
---
+++ KniffelBackend/src/main/java/KniffelProjekt/Kniffel/KniffelService.java
@@ -0,0 +1,20 @@
+package KniffelProjekt.Kniffel;
+
+import KniffelProjekt.Spieler.Spieler;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class KniffelService {
+
+    ArrayList<Kniffel> spielliste = new ArrayList<>();
+
+    public Kniffel spielStarten(ArrayList<Spieler> teilnehmer) {
+        Kniffel neuesKniffel = new Kniffel();
+        neuesKniffel.setId(System.currentTimeMillis());
+        neuesKniffel.setTeilnehmer(teilnehmer);
+        spielliste.add(neuesKniffel);
+        return neuesKniffel;
+    }
+}
