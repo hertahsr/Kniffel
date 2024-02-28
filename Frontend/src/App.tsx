d0683f135dbaaf803524ae270a60140335e50ddc
hertahsr <hertahsr@proton.me>
2024-02-28 11:49:02 +0100

    Replaced button from react vite template

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -18,9 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+          <MyButton />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,7 +26,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-        <MyButton />
+
     </>
   )
 }
